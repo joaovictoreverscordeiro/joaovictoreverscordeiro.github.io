a9964ff243b7a97185e439d5dc85946f27bb2196
joaovictoreverscordeiro <joaovictorevers@gmail.com>
2025-12-25 17:30:34 -0300

    incorporação js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
 /*
-Autor: João Victor Evers C.
-Script feito em 13/12/2025
+Script que roda a lista de desafios da página inicial
 */
 
+const num = 2;
 
-document.getElementById("texto").innerHTML="Hello, World!"
+var n = 1;
+document.getElementById("contador").innerHTML=`Página ${n}/${num}`;
+
+function anterior(){
+    if (n > 1) {
+        n--
+        document.getElementById("contador").innerHTML=`Página ${n}/${num}`;
+        document.getElementById(`pag${n}`).classList.remove("hidden");
+        document.getElementById(`pag${n+1}`).classList.add("hidden");
+    }
+    else{
+        n = num;
+        document.getElementById("contador").innerHTML=`Página ${n}/${num}`;
+        document.getElementById(`pag${n}`).classList.remove("hidden");
+        document.getElementById(`pag1`).classList.add("hidden");
+    }
+}
+
+function proxima(){
+    if (n < num){
+        n++
+        document.getElementById("contador").innerHTML=`Página ${n}/${num}`;
+        document.getElementById(`pag${n}`).classList.remove("hidden");
+        document.getElementById(`pag${n-1}`).classList.add("hidden");
+    }
+    else{
+        n = 1;
+        document.getElementById("contador").innerHTML=`Página ${n}/${num}`;
+        document.getElementById(`pag${n}`).classList.remove("hidden");
+        document.getElementById(`pag${num}`).classList.add("hidden");
+    }
+}
