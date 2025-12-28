f08d00f416bfd097a9d7f314a421ee2e28dc3193
joaovictoreverscordeiro <joaovictorevers@gmail.com>
2025-12-28 19:11:19 -0300

    atualização interna

--- js/simulador1.js
+++ js/simulador1.js
@@ -1,6 +1,85 @@
-var terminais = ["", ""]
+var terminaisMult = ["", ""];
+var terminaisCirc = ["", ""];
+var escala = "";
+const COMv = document.getElementById("salvarCOM");
+const _10Av = document.getElementById("salvar10A");
+const VRAv = document.getElementById("salvarVRA");
+
+/*
+Funções para botões
+ */
 
 function salvarCOM(){
-    terminais[0] = "COM"
-    console.log(terminais[0])
+    if (terminaisMult[0] === "COM"){
+        terminaisMult[0] = ""
+        console.log(terminaisMult[0])
+        document.getElementById("pontaNeg").innerHTML = ""
+        COMv.style.backgroundColor = "rgb(10, 35, 67)"
+    }
+    else{
+        terminaisMult[0] = "COM"
+        console.log(terminaisMult[0])
+        document.getElementById("pontaNeg").innerHTML = "COM"
+        COMv.style.backgroundColor = "rgb(58, 74, 97)"
+    }
 }
+
+function salvar10A(){
+    if (terminaisMult[1] === "10A"){
+        terminaisMult[1] = ""
+        console.log(terminaisMult[1])
+        document.getElementById("pontaPos").innerHTML = ""
+        _10Av.style.backgroundColor = "rgb(10, 35, 67)"
+    }
+    else if (terminaisMult[1] === "VRA"){
+        VRAv.style.backgroundColor = "rgb(10, 35, 67)"
+        terminaisMult[1] = "10A"
+        console.log(terminaisMult[1])
+        document.getElementById("pontaPos").innerHTML = "10A"
+        _10Av.style.backgroundColor = "rgb(58, 74, 97)"
+    }
+    else{
+        terminaisMult[1] = "10A"
+        console.log(terminaisMult[1])
+        document.getElementById("pontaPos").innerHTML = "10A"
+        _10Av.style.backgroundColor = "rgb(58, 74, 97)"
+    }
+}
+
+function salvarVRA(){
+    if (terminaisMult[1] === "VRA"){
+        terminaisMult[1] = ""
+        console.log(terminaisMult[1])
+        document.getElementById("pontaPos").innerHTML = ""
+        VRAv.style.backgroundColor = "rgb(10, 35, 67)"
+    }
+    else if (terminaisMult[1] === "10A"){
+        _10Av.style.backgroundColor = "rgb(10, 35, 67)"
+        document.getElementById("pontaPos").innerHTML = "V&Omega;&mu;mA"
+        terminaisMult[1] = "VRA"
+        console.log(terminaisMult[1]) 
+        VRAv.style.backgroundColor = "rgb(58, 74, 97)"
+    }
+    else{
+        document.getElementById("pontaPos").innerHTML = "V&Omega;&mu;mA"
+        terminaisMult[1] = "VRA"
+        console.log(terminaisMult[1]) 
+        VRAv.style.backgroundColor = "rgb(58, 74, 97)"
+    }
+}
+
+/* + funções para o circuito ( a desenvolver ) */
+
+function measure(){
+    if (terminaisMult[0] !== "COM" || terminaisMult[1] === ""){
+        alert("Não foi possível coletar a medida, multímetro conectado errado.")
+    }
+    else{
+        console.log(terminaisMult, terminaisCirc, escala)
+    }
+}
+
+/*
+Script para rodar o cursor do multímetro
+*/
+
