53209d561bc4361ca8023411a7981caf2a81246e
joaovictoreverscordeiro <joaovictorevers@gmail.com>
2025-12-28 19:45:09 -0300

    interatividade exp1

--- js/simulador1.js
+++ js/simulador1.js
@@ -68,6 +68,54 @@ function salvarVRA(){
     }
 }
 
+function salvarEsc(n){
+    if (escala === ""){
+        escala = `escala${n}`
+        console.log(escala)
+        document.getElementById(escala).style.backgroundColor = "rgb(20, 52, 96)"
+    }
+    else{
+        document.getElementById(escala).style.backgroundColor = "rgb(8, 27, 54)"
+        escala = `escala${n}`
+        console.log(escala)
+        document.getElementById(escala).style.backgroundColor = "rgb(20, 52, 96)"
+    }
+
+    if (1 <= n && n <=3){
+        document.getElementById("visorEsc").innerHTML = "V"
+    }
+    else if (n === 4){
+        document.getElementById("visorEsc").innerHTML = "&mu;A"
+    }
+    else if (n === 5){
+        document.getElementById("visorEsc").innerHTML = "mA"
+    }
+    else if (n === 6 || n === 7){
+        document.getElementById("visorEsc").innerHTML = "A"
+    }
+    else if (n === 8){
+        document.getElementById("visorEsc").innerHTML = "&Omega;"
+    }
+    else if (n === 9 || n === 10){
+        document.getElementById("visorEsc").innerHTML = "k&Omega;"
+    }
+    else if (n === 11){
+        document.getElementById("visorEsc").innerHTML = "M&Omega;"
+    }
+}
+
+function off(){
+    terminaisMult = ["", ""];
+    terminaisCirc = ["", ""];
+    document.getElementById("visorValor").innerHTML = ""
+    document.getElementById("visorEsc").innerHTML = ""
+
+    if (escala !== ""){
+        document.getElementById(escala).style.backgroundColor = "rgb(8, 27, 54)"
+        escala = ""
+    }
+}
+
 /* + funções para o circuito ( a desenvolver ) */
 
 function measure(){
